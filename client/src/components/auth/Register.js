74e1488f1a0a80e567e6da06c691f4f90ba4305b
unknown <savinraul1983@yahoo.com>
2018-08-23 11:37:10 +0100

    Redux action workflow

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
-export default class Register extends Component {
+class Register extends Component {
   constructor() {
     super();
     this.state = {
@@ -31,10 +34,11 @@ export default class Register extends Component {
       password2: this.state.password2
     };
 
-    axios
-    .post('/api/users/register', newUser)
-    .then(res => console.log(res.data))
-    .catch(err => this.setState({errors: err.response.data}));
+    // axios
+    // .post('/api/users/register', newUser)
+    // .then(res => console.log(res.data))
+    // .catch(err => this.setState({errors: err.response.data}));
+    this.props.registerUser(newUser);
 
   }
 
@@ -107,3 +111,14 @@ export default class Register extends Component {
     )
   }
 }
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { registerUser })(Register);
