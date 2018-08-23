795bf26d541cb924e32d21f1814c14f5cf4fe45a
unknown <savinraul1983@yahoo.com>
2018-08-23 11:03:30 +0100

    redux boiler-plate

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router,Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 //Css
 import './App.css';
+
 //Components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -13,17 +16,19 @@ import Login from './components/auth/Login';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className='App'>
-          <Navbar />
-            <Route exact path='/' component={ Landing } />
-            <div className='container'>
-              <Route exact path='/register' component={ Register } />
-              <Route exact path='/login' component={ Login } />
-            </div>
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={ store }>
+        <Router>
+          <div className='App'>
+            <Navbar />
+              <Route exact path='/' component={ Landing } />
+              <div className='container'>
+                <Route exact path='/register' component={ Register } />
+                <Route exact path='/login' component={ Login } />
+              </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
