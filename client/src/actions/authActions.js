872a788037637041ecde564d7fac57747ea5d83b
unknown <savinraul1983@yahoo.com>
2018-09-09 11:37:20 +0100

    set token to header

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
-
-import { GET_ERRORS } from './types'
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from './types'
 
 
 //Register User
@@ -28,9 +28,21 @@ export const loginUser = (userData) => dispatch => {
       localStorage.setItem('jwtToken', token);
       //Set token to Auth header
       setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
     }));
+};
+
+//Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
