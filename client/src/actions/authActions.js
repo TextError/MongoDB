1d964ca850c9352c6f4266e394ad5daaa1cfb502
unknown <savinraul1983@yahoo.com>
2018-09-09 13:04:33 +0100

    logout

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -46,3 +46,13 @@ export const setCurrentUser = (decoded) => {
     payload: decoded
   }
 }
+
+//Log user out
+export const logoutUser = () => dispatch => {
+  //remove token from localstorage
+  localStorage.removeItem('jwtToken');
+  //remove auth header for  future request
+  setAuthToken(false);
+  //set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+}
