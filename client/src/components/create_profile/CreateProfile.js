e65879cced1d7cb62062f37860f5e6e157117bc4
unknown <savinraul1983@yahoo.com>
2018-09-11 11:23:21 +0100

    profile inputs

--- client/src/components/create_profile/CreateProfile.js
+++ client/src/components/create_profile/CreateProfile.js
@@ -28,7 +28,33 @@ class CreateProfile extends Component {
     }
   }
 
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit')
+  }
+
   render() {
+
+    //Select option for status
+    const options = [
+      { label: '* Select Professional Status', value: 0},
+      { label: 'Developer', value: 'Developer' },
+      { label: 'Senior Developer', value: 'Senior Developer' },
+      { label: 'Junior Developer', value: 'Junior Developer' },
+      { label: 'Manager', value: 'Manager' },
+      { label: 'Student or Learning', value: 'Student or Learning' },
+      { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
+      { label: 'Intern', value: 'Intern' },
+      { label: 'Other', value: 'Other' }
+    ];
+    const { errors } = this.state
+
     return (
       <div className='create-profile'>
          <div className='container'>
@@ -37,6 +63,73 @@ class CreateProfile extends Component {
               <h1 className='display-4 text-center'>Create Your Profile</h1>
               <p className='lead text-center'>Let`s get some information to make your profile stand out</p>
               <small className='d-block pb-3'>* = require fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup 
+                  placeholder= '* Profile Handle'
+                  name='handle'
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info='A unique handle for your profile URL. Your full name, Company name, nickname.'
+                />
+                <SelectListGroup 
+                  placeholder= 'Status'
+                  name='status'
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info='Give us an idea of where you are in your career'
+                />
+                <TextFieldGroup 
+                  placeholder= 'Company'
+                  name='company'
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                  info='Could be your owm company or one you work for'
+                />
+                <TextFieldGroup 
+                  placeholder= 'Website'
+                  name='website'
+                  value={this.state.website}
+                  onChange={this.onChange}
+                  error={errors.website}
+                  info='Could be your own website or company one'
+                />
+                <TextFieldGroup 
+                  placeholder= 'Location'
+                  name='location'
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info='City or city & state suggested (eg. Boston, MA)'
+                />
+                <TextFieldGroup 
+                  placeholder= '* Skills'
+                  name='skills'
+                  value={this.state.skills}
+                  onChange={this.onChange}
+                  error={errors.skills}
+                  info='Please use comma separated values (eg. HTML, CSS, JAVASCRIPT, PHP)'
+                />
+                <TextFieldGroup 
+                  placeholder= 'Github Username'
+                  name='githubusername'
+                  value={this.state.githubusername}
+                  onChange={this.onChange}
+                  error={errors.githubusername}
+                  info='If you want your last repos and a Github link, include your username'
+                />
+                <TextAreaFieldGroup
+                  placeholder= 'Short bio'
+                  name='bio'
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info='Tell us a litlle about yourself'
+                />
+              </form>
             </div>
           </div>
          </div>
